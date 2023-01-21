64314dd19f554c5d21d74d802e63243343464ffa
KarthikTamil7 <108778191+KarthikTamil7@users.noreply.github.com>
2023-01-21 17:42:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import getenv
 
 class Config(object):
-      API_HASH = getenv("API_HASH", 5264bf4663e9159565603522f58d3c18)
+      API_HASH = getenv("API_HASH", "5264bf4663e9159565603522f58d3c18")
       API_ID = int(getenv("API_ID", 11973721))
       AS_COPY = True if getenv("AS_COPY", False) == "True" else False
       BOT_TOKEN = getenv("BOT_TOKEN", "5949999646:AAGNAzsUTMutsqtnSk2R4MkGgCQ0uMtqbIU")
