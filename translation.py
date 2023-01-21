652f22a7bc74f9c73d844143ece420d97bc7a737
KarthikTamil7 <108778191+KarthikTamil7@users.noreply.github.com>
2023-01-21 08:13:24 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,7 +3,11 @@ class Translation(object):
       # Start text
       START = "Hi I am a channel auto forward bot clone of <a href='https://t.me/Autoforward_Nsbot'>Channel Automatic NS bot</a>. \nMake your own bot now 👉 Source code \n\n<b>Support Channel and Groups:</b>\n @Ns_bot_updates, @Ns_Bot_supporters"             
 
-      #About text
+
+      # Help text
+      ABOUT = """A Simple telegram bot written in python to forward your files from one channel to other channel when a new post is available. What you need to do is just deploy this code."""
+
+      # About text
       ABOUT = """
 <b>📝 Language:</b> Python 3
 <b>🧰 Framework:</b> Pyrogram
