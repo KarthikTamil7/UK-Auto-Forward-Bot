34433d99a988a7f8e605ff42d3fa3dbbabcf8b1e
KarthikTamil7 <108778191+KarthikTamil7@users.noreply.github.com>
2023-01-24 19:14:17 +0530

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -1,4 +1,6 @@
 import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 import asyncio
