f71d48d0511c0717ab55dfbbbee0df6a9c87acde
KarthikTamil7 <108778191+KarthikTamil7@users.noreply.github.com>
2023-01-24 20:41:36 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -12,7 +12,8 @@ from translation import Translation
 
 @channelforward.on_message(filters.command("start") & filters.private & filters.incoming)
 async def start(client, message):
-    await message.reply(
+    await message.reply_photo(
+        photo=random.choice(PICS),
         text=Translation.START,
         disable_web_page_preview=True,
         quote=True
